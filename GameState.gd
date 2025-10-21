34bde705e0a92a0c45478557f3aa036c4e6892ce
ThingsAreCoolSometimes <griffingamescience@gmail.com>
2025-10-21 14:38:56 -0400

    Fixed Mouse positioning, fixed texture sizes, removed old broken web, made numbers format more legibly

--- GameState.gd
+++ GameState.gd
@@ -1,12 +1,9 @@
 # GameState.gd
 extends Node
 
-# --- Player Stats ---
 var squirrels: float = 0.0
 var squirrels_per_click: int = 1
 
-# --- Building Modifiers ---
-# We'll use these later to apply upgrades like "Better Nuts"
+
 var nut_sps_bonus: float = 0.0
 var tree_sps_bonus: float = 0.0
-# ... add more for other buildings
