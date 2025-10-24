c2ae3cfa19354bd634bc42463adb843d137383fc
Griffin Crooks Bourassa <griffingamescience@gmail.com>
2025-10-24 11:51:28 -0400

    Fixed Cursor and menu errors

--- animated_cursor.gd
+++ animated_cursor.gd
@@ -3,32 +3,28 @@ extends AnimatedSprite2D
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	# Start in the idle state
 	play("idle")
-	# Connect the signal so we know when an animation is done
 	animation_finished.connect(_on_animation_finished)
 
-func _process(delta):
-	# This part is perfect, keep it as is
+func _process(_delta):
+	if not is_inside_tree():
+		return
 	global_position = get_global_mouse_position()
 
 func _input(event):
-	# We only care about the left mouse button
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		
-		# If the button was just PRESSED
 		if event.is_pressed():
-			# Play the "click_down" animation once.
-			play("click_down")
+			play("clickDown")
 			
 		# If the button was just RELEASED
 		else:
 			# Play the "click_up" animation once.
-			play("click_up")
+			play("clickUp")
 
 # This function is called automatically when any non-looping animation finishes.
 func _on_animation_finished():
 	# If the animation that just finished was our "click_up" animation...
-	if animation == "click_up":
+	if animation == "clickUp":
 		# ...then it's time to go back to the idle state.
 		play("idle")
